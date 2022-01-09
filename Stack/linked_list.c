c8794f041935a83476003feb58427e69c0c05dbd
SAKTHISPARROW <89923770+SAKTHISPARROW@users.noreply.github.com>
2022-01-09 11:46:10 +0530

    Update linked_list.c

--- Stack/linked_list.c
+++ Stack/linked_list.c
@@ -1,104 +1,105 @@
-#include <stdio.h>
-#include <malloc.h>
-
-#define MAX 10
+#include<stdio.h>
+#include<stdlib.h>
+#define max 10
+struct node
+{
+    int data;
+    struct node* link;
+};
+struct node* head;
 int top;
 
-struct node{
-    int data;
-    struct node *next;
-};
-
-struct node *head;
-
-void push(int data){    
-    if(top==MAX-1){
-        printf("Stack Overflow\n");
+void push(int data)
+{
+    if(top==max-1)
+    {
+        printf(" Stack Overflow\n");
         return;
     }
-  
-    struct node *newnode=(struct node *)malloc(sizeof(struct node));
+    struct node* newnode=malloc(sizeof(newnode));
     newnode->data=data;
-    newnode->next=head;
-
-    if(top==-1){
+    newnode->link=head;
+    if(top==-1)
+    {
         head=newnode;
         top++;
         return;
     }
-
-    newnode->next=head;
     head=newnode;
     top++;
-
 }
 
-void pop(){
-    struct node *temp=head;
-    if(top==-1){
-        printf("Stack Underflow\n");
+void pop()
+{
+    if(top==-1)
+    {
+        printf(" Stack Underflow\n");
         return;
     }
-    printf("\n%d\n\n",temp->data);
-
-    head=head->next;
+    struct node* temp=head; 
+    printf(" Poped data :%d\n",temp->data);
+    head=head->link;
     free(temp);
 }
 
-void peek(){
-    if(top==-1){
-        printf("Stack Underflow\n");
-        return;
+void peek()
+{
+    if(top==-1)
+    {
+        printf(" Stack Underflow\n");
+        return;       
     }
-    printf("\n%d\n\n",head->data);
+    printf(" Topmost data :%d\n",head->data);
 }
-
-void display(){
-    if(top==-1){
-        printf("Stack Underflow\n");
-        return;
+void display()
+{
+    if(top==-1)
+    {
+        printf(" Stack Underflow\n");
+        return;       
     }
-    struct node *temp=head;
-    while(temp!=NULL){
-        printf("\n%d\n|",temp->data);
-        temp=temp->next;
+    struct node* temp=head;
+    while (temp!=NULL)
+    {
+        printf(" Stack elements :%d\n",temp->data);
+        temp=temp->link;
     }
-    printf("NULL\n");
+        
 }
-
-int main(){
+int main()
+{
     head=NULL;
-    top = -1;
+    top=-1;
     int choice;
-    do{
-    printf("\nChoice\n");
-    printf("1.Push\n");
-    printf("2.Pop\n");
-    printf("3.Display\n");
-    printf("4.Peek\n");
-    printf("5.Quit\n");
-    printf("Enter choice: ");
-    scanf("%d",&choice);
-    switch(choice){
+    do
+    {
+        printf(" 1.Push\n 2.Pop\n 3.Peek\n 4.Display\n 5.Quit\n ");
+        printf("Enter your choice :",choice);
+        scanf("%d",&choice);
+        switch (choice)
+        {
         case 1:
-            printf("Enter data: ");
             int data;
+            printf(" Enter the data to be pushed : ",data);
             scanf("%d",&data);
+            printf(" %d is pushed\n",data);
             push(data);
             break;
         case 2:
             pop();
             break;
         case 3:
-            display();
-            break;
-        case 4:
             peek();
             break;
-        case 5:
+        case 4:
+            display();
             break;
+        case 5:
+            exit(1);
         default:
-            printf("Invalid choice\n");
-    }
-    }while (choice!=5);
+            printf(" Please Enter valid choice");
+            break;
+        }
+    } while (choice!=5);
+    return 0;
 }
